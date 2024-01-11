120f86c7369e5a0f7b8af746195b7ed07c9907d5
woozoolinux <magiccolor2@gmail.com>
2024-01-11 05:39:43 +0100

    v2

--- LDS_check.sh
+++ LDS_check.sh
@@ -18,9 +18,59 @@ LDS_HOME=/home/LDS
 REPORT_FILE="${LDS_HOME}/OS_Monthly_Report_${HOSTNAME}_${DATE}"
 
 # Version Check
-RHEL_VERSION=`cat /etc/redhat-release | grep -v ^# | awk '{print $7}' | cut -d. -f1`
+OS_VERSION=`cat /etc/redhat-release | grep -v ^# | awk '{print $(NF-1)}' | cut -d. -f1`
 
 # check MAIN PATH
 if [ ! -d  "$LDS_HOME" ]; then
         mkdir -p $LDS_HOME
 fi
+
+Hostname()
+{
+echo "=== HostName Check ==="
+echo
+
+hostname
+
+echo
+echo "===  End HostName ==="
+echo
+}
+
+OsVersion()
+{
+echo "=== OsVersion Check ==="
+echo
+
+cat /etc/redhat-release
+
+echo
+echo "=== End OsVersion ==="
+echo
+}
+
+FileSystem()
+{
+echo
+echo "=== FileSystem Check ==="
+echo 
+
+echo "1. df -h result:"
+df -h | grep -v tmpfs
+echo
+
+echo "2. df -i result:"
+df -i | grep -v tmpfs
+echo
+
+echo "=== End FileSystem ==="
+}
+
+main()
+{
+Hostname
+OsVersion
+FileSystem
+}
+
+main
