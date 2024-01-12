39c813691387d5e222698b38cee8b144fe5419ec
woozoolinux <magiccolor2@gmail.com>
2024-01-12 09:39:35 +0100

    v5

--- initial_RHEL9.sh
+++ initial_RHEL9.sh
@@ -17,8 +17,16 @@ OS_VERSION=`cat /etc/redhat-release | grep -v ^# | awk '{print $(NF-1)}' | cut -
 
 
 # Variable SET
-NTP_Address="111.15.30.23 11.22.33.44" 
+
+## SET NTP ADDRESS
+## ex) NTP_Address="123.123.123.1 123.123.123.2"
+NTP_Address="111.15.30.23"
+
+## Enter the package name you want to install
+## ex) Package_list="net-tools sysstat"
 Package_list="net-tools sysfsutils pciutils sysstat traceroute createrepo sos lvm2 java-1.8.0-openjdk-devel"
+
+
 
 NTP()
 {
@@ -26,11 +34,18 @@ NTP()
 # Diable default chrony pool
 sed -i  "/^pool/s/pool/#pool/" /etc/chrony.conf
 
-# Insert time server
-for i in $NTP_Address
-do
-	echo "server ${i} iburst" >> /etc/chrony.conf
-done
+# Set time server
+if [ -z ${NTP_Address} ];
+then
+        echo "NTP WARNING!!"
+        echo "NO NTP ADDRESS, PLEASE SET NTP VARIABLE!!"
+else
+        for i in $NTP_Address
+        do
+        	echo "server ${i} iburst" >> /etc/chrony.conf
+        done
+fi
+
 
 # start and enable chronyd service
 systemctl restart chronyd
